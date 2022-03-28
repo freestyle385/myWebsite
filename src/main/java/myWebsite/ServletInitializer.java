40f6f4e59e8a075b3f83dc6d2e45163be1319402
freestyle385 <freestyle385@gmail.com>
2022-03-28 22:15:34 +0900

    메이븐 빌드로 배포 시 에러페이지 필터를 비활성화, 404 에러 방지

--- src/main/java/myWebsite/ServletInitializer.java
+++ src/main/java/myWebsite/ServletInitializer.java
@@ -4,9 +4,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		//set register error pagefilter false
+        setRegisterErrorPageFilter(false);
+		
 		return application.sources(MyWebsiteApplication.class);
 	}
 
